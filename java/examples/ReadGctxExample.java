8decf912c8a01313ca7a50efbceab479c9e1ca38
joshua-gould <jgould@broadinstitute.org>
2015-02-06 07:54:08 -0500

    added gctx writer

--- java/examples/ReadGctxExample.java
+++ java/examples/ReadGctxExample.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import cmap.io.GctxReader;
+import cmap.io.GctxWriter;
 import cmap.matrix.Dataset;
 import cmap.matrix.Vector;
 
@@ -12,7 +13,7 @@ public class ReadGctxExample {
 	try {
 	    // read the full dataset
 	    Dataset dataset = reader.read();
-	    System.out.println(dataset.getRowCount() + " rows, " + dataset.getColumnCount() + " columns");
+	    System.out.println("read " + dataset.getRowCount() + " rows, " + dataset.getColumnCount() + " columns");
 	    Vector columnIds = dataset.getColumnMetadata().get("id");
 
 	    // read the 1st 10 column ids
@@ -22,7 +23,12 @@ public class ReadGctxExample {
 		columnIdsToExtract.add((String) columnIds.getValue(j));
 	    }
 	    dataset = reader.read(null, columnIdsToExtract);
-	    System.out.println(dataset.getRowCount() + " rows, " + dataset.getColumnCount() + " columns");
+	    System.out.println("read " + dataset.getRowCount() + " rows, " + dataset.getColumnCount() + " columns");
+
+	    // write out the subset
+	    new GctxWriter("slice.gctx", dataset);
+	    System.out.println("wrote " + dataset.getRowCount() + " rows, " + dataset.getColumnCount()
+		    + " columns to slice.gctx");
 
 	} finally {
 	    reader.close();
